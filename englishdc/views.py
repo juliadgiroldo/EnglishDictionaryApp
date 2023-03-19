198a9e73128a5e8759c3dc0210af84bd300264e2
Julia <donadellij@gmail.com>
2023-03-19 20:13:44 -0300

    criando views e templates

---
+++ englishdc/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from PyDictionary import PyDictionary 
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def word(request):
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    antonyms = dictionary.antonym(search)
+    synonyms = dictionary.synonym(search)
+
+    context = {
+
+        'meaning': meaning['Noun'][0],
+        'antonyms': antonyms,
+        'synonyms': synonyms,
+    }
+
+    return render(request, 'word.html', context)
